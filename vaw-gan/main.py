fedd7d52052e2dd4c5d2305b5eb804b3efd858ff
ankitrgupta1 <ankitrgupta1@gmail.com>
2019-04-28 23:58:19 -0400

    Committing the vaw-gan

---
+++ vaw-gan/main.py
@@ -0,0 +1,77 @@
+import json
+import os
+
+from importlib import import_module
+
+import numpy as np
+import tensorflow as tf
+
+from analyzer import Tanhize, read
+from util.wrapper import validate_log_dirs
+
+args = tf.app.flags.FLAGS
+tf.app.flags.DEFINE_string(
+    'logdir_root', None, 'root of log dir')
+tf.app.flags.DEFINE_string(
+    'logdir', None, 'log dir')
+tf.app.flags.DEFINE_string(
+    'restore_from', None, 'restore from dir (not from *.ckpt)')
+tf.app.flags.DEFINE_string('gpu_cfg', None, 'GPU configuration')
+tf.app.flags.DEFINE_integer('summary_freq', 1000, 'Update summary')
+tf.app.flags.DEFINE_string(
+    'ckpt', None, 'specify the ckpt in restore_from (if there are multiple ckpts)')  # TODO
+tf.app.flags.DEFINE_string(
+    'architecture', 'architecture-vawgan-vcc2016.json', 'network architecture')
+
+tf.app.flags.DEFINE_string('model_module', 'model.vae', 'Model module')
+tf.app.flags.DEFINE_string('model', 'ConvVAE', 'Model: ConvVAE, VAWGAN')
+
+tf.app.flags.DEFINE_string('trainer_module', 'trainer.vae', 'Trainer module')
+tf.app.flags.DEFINE_string('trainer', 'VAETrainer', 'Trainer: VAETrainer, VAWGANTrainer')
+
+def main(unused_args):
+    ''' NOTE: The input is rescaled to [-1, 1] '''
+
+    if args.model is None or args.trainer is None:
+        raise ValueError(
+            '\n  Both `model` and `trainer` should be assigned.' +\
+            '\n  Use `python main.py --help` to see applicable options.'
+        )
+
+    module = import_module(args.model_module, package=None)
+    MODEL = getattr(module, args.model)
+
+    module = import_module(args.trainer_module, package=None)
+    TRAINER = getattr(module, args.trainer)
+
+    dirs = validate_log_dirs(args)
+    tf.gfile.MakeDirs(dirs['logdir'])
+
+    with open(args.architecture) as f:
+        arch = json.load(f)
+
+    with open(os.path.join(dirs['logdir'], args.architecture), 'w') as f:
+        json.dump(arch, f, indent=4)
+
+    normalizer = Tanhize(
+        xmax=np.fromfile('./etc/xmax.npf'),
+        xmin=np.fromfile('./etc/xmin.npf'),
+    )
+
+    x, y = read(
+        file_pattern=arch['training']['datadir'],
+        batch_size=arch['training']['batch_size'],
+        capacity=4096,
+        min_after_dequeue=3000,
+        normalizer=normalizer,
+    )
+
+    machine = MODEL(arch, is_training=True)
+
+    loss = machine.loss(x, y)
+    trainer = TRAINER(loss, arch, args, dirs)
+    trainer.train(nIter=arch['training']['max_iter'], machine=machine)
+
+
+if __name__ == '__main__':
+    tf.app.run()
